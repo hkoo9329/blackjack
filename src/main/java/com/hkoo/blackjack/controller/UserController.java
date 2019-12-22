a907e1690bf9ff6cc45ecf72fb56f4d72794145f
hkoo <tyrfingr12@naver.com>
2019-12-22 19:43:24 +0900

    user 생성을 통한 repo controller domain 구현

--- src/main/java/com/hkoo/blackjack/controller/UserController.java
+++ src/main/java/com/hkoo/blackjack/controller/UserController.java
@@ -1,4 +1,22 @@
-package com.hkoo.blackjack.controller.api;
+package com.hkoo.blackjack.controller;
 
+
+import com.hkoo.blackjack.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@Slf4j
 public class UserController {
+
+    @GetMapping("/myself")
+    public User getMyself(@AuthenticationPrincipal User user){
+        log.info("{}",user);
+        return user;
+    }
+
 }
--- src/main/java/com/hkoo/blackjack/domain/User.java
+++ src/main/java/com/hkoo/blackjack/domain/User.java
@@ -1,4 +1,18 @@
 package com.hkoo.blackjack.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    private String name;
+    private long account;
 }
