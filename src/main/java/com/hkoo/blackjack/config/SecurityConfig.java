6aa6f6344daea75934295e8e4a11d41524ad4f59
hkoo <tyrfingr12@naver.com>
2019-12-22 19:58:26 +0900

    security 설정 추가'

--- src/main/java/com/hkoo/blackjack/config/SecurityConfig.java
+++ src/main/java/com/hkoo/blackjack/config/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.hkoo.blackjack.config;
 
-public class SecurityConfig {
+import com.hkoo.blackjack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.web.filter.CompositeFilter;
+
+import java.util.Arrays;
+import java.util.logging.Filter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and().csrf().disable()
+                .antMatcher("/**").authorizeRequests()
+                .antMatchers("/", "/view/**", "/login**", "/webjars/**", "/error**" ,"/blackjack/**", "/h2-console/**")
+                .permitAll().anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll()
+                .and()
+                .headers().frameOptions().disable()
+    }
+    private Filter ssoFilter(){
+        CompositeFilter compositeFilter = new CompositeFilter();
+        compositeFilter.setFilters(Arrays.asList());
+    }
 }
